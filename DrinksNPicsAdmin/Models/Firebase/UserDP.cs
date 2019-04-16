c4ddc5bb16c2e5ea5c5af2652637ed18ca38adbe
Hercules <hercules@polispay.org>
2019-04-16 18:26:51 -0500

    Login page with controller working.

---
+++ DrinksNPicsAdmin/Models/Firebase/UserDP.cs
@@ -0,0 +1,14 @@
+namespace DrinksNPicsAdmin.Models.Firebase
+{
+    public class UserDP
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string username { get; set; }
+        
+        public UserDP()
+        {
+            
+        }
+    }
+}
---
+++ DrinksNPicsAdmin/Services/FirebaseService.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using DrinksNPicsAdmin.Models.Firebase;
+
+
+namespace DrinksNPicsAdmin.Services
+{
+    public class FirebaseService
+    {
+        private const string APIKEY = "AIzaSyCSv35d6_nlMBkvVUlxTPYcHEmGWCVPmpg";
+        private const string auth = "2EYmkOQ3GbMUlHVV8W0WtPSCsBrrYjpBERbC2FMz";
+
+        public FirebaseClient firebaseClient { get; set; }
+
+
+        public FirebaseService()
+        {
+            firebaseClient = new FirebaseClient("https://drinksnpics.firebaseio.com/",
+            new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(auth)
+            });
+        }
+        
+        public async Task<UserDP> getUser(string username)
+        {
+            UserDP _user = new UserDP();
+            var indexs = await firebaseClient                
+                .Child("users")
+                .OrderByKey()
+                .EqualTo(username)
+                .OnceAsync<UserDP>();
+
+            foreach (var index in indexs)
+            {
+                _user = index.Object;
+                return _user;
+            }
+            return null;
+        }
+    }
+}
